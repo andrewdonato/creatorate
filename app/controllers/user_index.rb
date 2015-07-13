bf54b3cbd8b3b686b96045a56486db51c9e7a5b2
[01;31m[K"name"[m[K: "Andrew Donato <[01;31m[K"email"[m[K: "andrew.donato91@gmail.com>
2015-07-13 01:25:13 -0700

    able to signup and add users to database

--- app/controllers/user_index.rb
+++ app/controllers/user_index.rb
@@ -1,13 +1,32 @@
+get '/' do
+  erb :home
+  # if current_user
+  #   erb :home
+  # else
+  #   p '@' * 90
+  #   p "not happy"
+  # end
+end
+
+get '/signout' do
+  logout
+  redirect '/'
+end
+
 ############ index ############
 get "/users" do
 end
 
 ############ new ############
 get "/users/new" do
+
 end
 
 ############ create ############
 post "/users" do
+  sign_up
+  login
+  redirect '/'
 end
 
 ############ show ############
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  include BCrypt
+
+
+
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
