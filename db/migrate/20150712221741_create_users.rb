455c017c7e33dd3c4a60e780c2cf7024bb566110
[01;31m[K"name"[m[K: "Andrew Donato <[01;31m[K"email"[m[K: "andrew.donato91@gmail.com>
2015-07-12 22:47:44 -0700

    created migrations

--- db/migrate/20150712221741_create_users.rb
+++ db/migrate/20150712221741_create_users.rb
@@ -1,4 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+
+      t.string :name
+      t.string :email
+      t.string :password_hash
+      t.string :skill_paragraph
+
+      t.timestamps
+
+    end
   end
 end
--- db/migrate/20150712221749_create_skills.rb
+++ db/migrate/20150712221749_create_skills.rb
@@ -1,4 +1,11 @@
 class CreateSkills < ActiveRecord::Migration
   def change
+    create_table :skills do |t|
+
+      t.string :skill_word
+
+      t.timestamps
+
+    end
   end
 end
