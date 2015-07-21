78cbdde2dcac3dae5a323389072c103499bca37f
[01;31m[K"name"[m[K: "Andrew Donato <[01;31m[K"email"[m[K: "andrew.donato91@gmail.com>
2015-07-21 14:38:46 -0700

    Creators collaborate.
    Creators are now automatically added as collaborators to projects they create.
    Next plan is to ajax the collaborators index on project page.
    Also need; creator deletes collaborators off of projects,
    collaborators can delete projects off the collaborations index.
    Future features, skill needs and skill search.

--- app/controllers/project_index.rb
+++ app/controllers/project_index.rb
@@ -20,12 +20,21 @@ post "/projects" do
     description: params[:description],
     needs: params[:needs]
   )
-  p "*" * 100
-  p params
-  p "*" * 100
+  @project_users = User.joins(:project_users).where('project_users.project_id' => @project.id)
+
   if @project.save
     # (erb :"project/project_index", layout: false).to_json  ## this is what I had before, it was going to be the whole list being reappended, not the best idea because it can be a lot of data.
     # (erb :"_listing", layout: false, locals: {project: @project }).to_json  ## this is an alternative way of rendering each listing in the index.
+    collaborator = ProjectUser.new(user_id: current_user.id, project_id: @project.id)
+    if collaborator.save
+      p "*" * 100
+      p 'creator saved as collaborator'
+      p "*" * 100
+    else
+      p "&" * 100
+      p 'creator NOT saved'
+      p "&" * 100
+    end
     (@project).to_json
   else
     p '%' * 50
@@ -37,6 +46,7 @@ end
 ############ show ############
 get "/projects/:id" do
   @project = Project.find_by(id: params[:id])
+
   # @users = Users.find
   # select * from project_users inner join users on project_users.user_id = users.id where project_users.project_id = params[:id]
   # @project_users = User.joins(:project_users).where(:project_id == @project.id)
@@ -44,13 +54,13 @@ get "/projects/:id" do
   @creator = User.find_by(id: "#{@project.creator_id}")
   @user = current_user
 
-  if @project_users
-    @project_users.each do |user|
-      p '%' * 50
-      p user
-      p '%' * 50
-    end
-  end
+  # if @project_users
+  #   @project_users.each do |user|
+  #     p '%' * 50
+  #     p user
+  #     p '%' * 50
+  #   end
+  # end
 
 
   erb :"project/project_show"
